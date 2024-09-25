88244c095a39862254f576dce944cf634a86348e
mtayyebdeveloper <tayyabkhank1234@gmail.com>
2024-09-25 08:25:59 -0700

    updated port

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 10000 // desired port number
+    host: '0.0.0.0',
+    port: 5000, // Or any other port number
   }
 })
